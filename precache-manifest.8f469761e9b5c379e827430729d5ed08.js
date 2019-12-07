168bc22d7e7ac26e9a06c67576ae8e6ca91c23ce
Tui Tpk <tanamet.tpk@gmail.com>
2019-12-07 10:52:50 +0700

    Updates

---
+++ precache-manifest.8f469761e9b5c379e827430729d5ed08.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a5302da6aeb686c1d440336d07d8df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e063ef91ffe5911ab5fe",
+    "url": "/static/css/main.3abda9b1.chunk.css"
+  },
+  {
+    "revision": "be11409ac0a45b3f8cf5",
+    "url": "/static/js/2.91476176.chunk.js"
+  },
+  {
+    "revision": "e063ef91ffe5911ab5fe",
+    "url": "/static/js/main.b66d9ad8.chunk.js"
+  },
+  {
+    "revision": "87cdc0a9b9df5332064f",
+    "url": "/static/js/runtime~main.b5bdb10a.js"
+  },
+  {
+    "revision": "92ccddf2ecba3ff5754ba8f2ebd8b1aa",
+    "url": "/static/media/Datefilter.92ccddf2.svg"
+  },
+  {
+    "revision": "a6ed1466d142610aade4124034151d33",
+    "url": "/static/media/Pin.a6ed1466.svg"
+  },
+  {
+    "revision": "c560de1fd0c39dbf4fe4c66e40c5dcca",
+    "url": "/static/media/PinP.c560de1f.svg"
+  },
+  {
+    "revision": "d072b762de234e54b759b3e216a0fb9e",
+    "url": "/static/media/Star.d072b762.svg"
+  },
+  {
+    "revision": "2f958964305485b00ff58a8181b8be04",
+    "url": "/static/media/StarP.2f958964.svg"
+  },
+  {
+    "revision": "650cdc57dc0be3a71cc230a760215df5",
+    "url": "/static/media/delete.650cdc57.svg"
+  }
+]);
